d00f01caf5640fbf1ef2632641fab297275c59ab
vasjko <vasjko@gmail.com>
2016-01-29 12:48:41 +0200

    Implementation of step finder algorithms: Add test path visualization

---
+++ ArtifactAdmin.Web/Controllers/TestPathController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using ArtifactAdmin.BL.Interfaces;
+
+namespace ArtifactAdmin.Web.Controllers
+{
+    using Models;
+
+    public class TestPathController : Controller
+    {
+        private IStepFinderService stepFinderService;
+
+        public TestPathController(IStepFinderService stepFinderService)
+        {
+            this.stepFinderService = stepFinderService;
+        }
+
+        // GET: TestPath
+        public ActionResult Index()
+        {
+            return View(new TestPathModel());
+        }
+
+        public ActionResult GetPoints()
+        {
+            //var pts = new List<Point>();
+            //Random rnd = new Random();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    pts.Add(new Point(rnd.Next(1000), rnd.Next(1000)));
+            //}
+
+            var pts = stepFinderService.GetPathPoints();
+
+            return Json(pts, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ArtifactAdmin.Web/Models/TestPathModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Windows;
+
+namespace ArtifactAdmin.Web.Models
+{
+    public class TestPathModel
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+        public double WMax { get; set; }
+        public double WMin { get; set; }
+        public double AMax { get; set; }
+        public double AMin { get; set; }
+        public double ProbMax { get; set; }
+        public double ProbMin { get; set; }
+        public double SkipPtsMax { get; set; }
+        public double SkipPtsMin { get; set; }
+    }
+}
